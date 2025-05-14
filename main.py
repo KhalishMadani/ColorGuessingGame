4a45963191d170d813c34cac645e6349a1b8d294
khalishmadani <khalis.madani@bic.co.id>
2025-05-14 12:46:29 +0800

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import eel
 import argparse
 import numpy as np
 import cv2
+import base64
 
 eel.init('web')
 
@@ -13,4 +14,21 @@ def sample_text():
 def receive_data(msg):
     print(msg)
 
+cam = cv2.VideoCapture(0)
+@eel.expose
+def open_cam():
+    ret, frame = cam.read()
+    if not ret:
+        return ""
+    _, buffer = cv2.imencode('.jpg', frame)
+    encoded = base64.b64encode(buffer).decode('utf-8')
+    print(f'------ 1 -------')
+    return f'data:image/jpeg;base64,{encoded}'
+
+# @eel.expose
+# def close_cam():
+#     return cam.release()
+    
+
 eel.start('index.html', size=(800, 600))
+cam.release()
--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,9 @@
 const h1Element = document.getElementById('h1-element');
 const getBtn = document.getElementById('get-py')
 const sendBtn = document.getElementById('send-js')
+const cameradBtn = document.getElementById('camera-btn')
+const imgCamLocation = document.getElementById('cam-img')
+const closeCamBtn = document.getElementById('close-cam-btn')
 
 
 getBtn.addEventListener('click', async () => {
@@ -10,3 +13,13 @@ getBtn.addEventListener('click', async () => {
 sendBtn.addEventListener('click', async () => {
     await eel.receive_data('Hello there from the web!!');
 });
+
+async function usecamera() {
+    imgCamLocation.src = await eel.open_cam()();
+}
+
+cameradBtn.onclick = async () => {
+    setInterval(usecamera(), 30);
+}
+
+// closeCamBtn.onclick = async () => await eel.close_cam()()
