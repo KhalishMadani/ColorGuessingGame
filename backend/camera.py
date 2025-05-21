ab14c19c92996573acc284c62e3210bd7ec668d4
Khalish Madani <150880977+MadaniKhalish@users.noreply.github.com>
2025-05-21 21:32:55 +0800

    update

--- backend/camera.py
+++ backend/camera.py
@@ -14,6 +14,27 @@ def open_cam():
     ret, frame = cam.read()
     if not ret:
         return ""
+    
+    height, width, _ = frame.shape
+    center_x = int(width/2)
+    center_y = int(height/2)
+    pixel_center = frame[center_y, center_x]
+    print(f"Pixel at center: {pixel_center}")
+
+    #custom circle
+    radius = 25
+    color = (255, 0, 0)
+    thickness = 2
+
+    
+    cv2.circle(
+        frame,
+        (center_x, center_y),
+        radius,
+        color,
+        thickness
+    )
+
     _, buffer = cv2.imencode('.jpg', frame)
     encoded = base64.b64encode(buffer).decode('utf-8')
     return f'data:image/jpeg;base64,{encoded}'
