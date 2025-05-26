674d28d6fbc0300406a28e28d3b73c47ea92cad6
Khalish Madani <khalishmadani112@gmail.com>
2025-05-26 13:25:15 +0800

    update

--- web/js/index.js
+++ web/js/index.js
@@ -1,17 +1,29 @@
 const toggleText = document.getElementById('toggle-text')
-const cameradBtn = document.getElementById('startcam-btn')
+const startCamBtn = document.getElementById('startcam-btn')
+const captureCamBtn = document.getElementById('capturecam-btn')
 const imgCamLocation = document.getElementById('cam-img')
 const closeCamBtn = document.getElementById('closecam-btn')
 
 let startCamInterval = null;
 
-cameradBtn.onclick = () => {
+startCamBtn.onclick = () => {
     if (startCamInterval) return;
     toggleText.style.display = 'none';
 
     startCamInterval = setInterval( async () => { 
         imgCamLocation.src = await eel.open_cam()();
     }, 50);
+
+    startCamBtn.style.display = 'none';
+    captureCamBtn.style.display = 'block';
+}
+
+captureCamBtn.onclick = async () => {
+    startCamBtn.style.display = 'block';
+    clearInterval(startCamInterval);
+    startCamInterval = null;
+    await eel.close_cam()();
+    captureCamBtn.style.display = 'none';
 }
 
 
@@ -21,4 +33,5 @@ closeCamBtn.onclick = async () => {
     await eel.close_cam()();
     toggleText.style.display = 'block';
     imgCamLocation.src = '';
+    startCamBtn.style.display = 'block';
 }
